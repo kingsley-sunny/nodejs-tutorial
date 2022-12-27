a834ffc1225d1fb7aaedf9d3106e2ed477486e1c
kingsley-sunny <ezeobisunny51@gmail.com>
2022-12-27 19:02:37 +0100

    learning express js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const http = require("http");
 const requestHandler = require("./routes");
+const express = require("express");
 
-const server = http.createServer(requestHandler);
-// console.log(os.freemem() / 1024, os.totalmem() / 1024);
+const app = express();
+
+// app.use is a middle ware
+app.use((req, res, next) => {
+    console.log("In the middle ware");
+    next(); // This allows the next functions to continue to another middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log("In the second middle ware");
+});
+
+console.log(app.use);
+
+const server = http.createServer(app);
 
 server.listen(3000);
