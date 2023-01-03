dfb04b0cdda24553d9f83802e9af1cc074025d37
kingsley-sunny <ezeobisunny51@gmail.com>
2023-01-03 23:09:11 +0100

    done my assignment

--- app.js
+++ app.js
@@ -1,23 +1,122 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const fs = require("fs");
+const http = require("http");
 
-const adminRoute = require("./routes/admin");
-const shopRoute = require("./routes/shop");
-const apiRoute = require("./routes/file");
+const usersData = [];
 
-const app = express();
+function handleRequest(req, res) {}
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: false }));
+const server = http.createServer((req, res) => {
+    const route = req.url;
+    const method = req.method;
 
-app.use("/api", apiRoute);
-app.use("/admin", adminRoute);
-app.use(shopRoute);
+    if (route === "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.statusCode = 200;
+        res.write(`<div>
+    <h1>Welcome to my first nodejs assignment</h1>
+    <br/>
+    <a href="/users"> view all the users of this application </a>
+    <br/>
+    <a href="/create-user"> create a user </a>
 
-// The 404 page
-app.use((req, res) => {
-    res.status(404).send("<h1>Page not found</h1>");
+    </div>`);
+        return res.end();
+    }
+
+    if (route === "/create-user" && method === "GET") {
+        res.setHeader("Content-Type", "text/html");
+        res.statusCode = 200;
+        res.write(`<form method="POST" action="/create-user">
+            <input type="text" name="username" />
+            <button type="submit">Create A User</button>
+        </form>`);
+        return res.end();
+    }
+
+    if (route === "/create-user" && method === "POST") {
+        req.on("data", chunk => {
+            const parsedBody = chunk.toLocaleString();
+            const data = parsedBody.split("=")[1];
+            usersData.push(data);
+
+            fs.readFile("users.json", (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                if (!data.toString()) {
+                    fs.writeFile("users.json", JSON.stringify(usersData), () => {});
+
+                    res.setHeader("Content-Type", "text/html");
+                    res.writeHead(301, { Location: "/users" });
+                    return res.end();
+                }
+
+                const parsedData = JSON.parse(data);
+                fs.writeFile(
+                    "users.json",
+                    JSON.stringify([...parsedData, ...usersData]),
+                    (err, dat) => {
+                        if (err) {
+                            console.log(err);
+                            return;
+                        }
+                        res.setHeader("Content-Type", "text/html");
+                        res.writeHead(301, { Location: "/users" });
+                        return res.end();
+                    }
+                );
+            });
+        });
+    }
+
+    if (route === "/users" && method === "GET") {
+        fs.readFile("users.json", (err, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (data.toString()) {
+                const parsedData = JSON.parse(data.toString());
+
+                let list = ``;
+
+                for (const data of parsedData) {
+                    list += `<li>${data}</li>`;
+                }
+
+                res.setHeader("Content-Type", "text/html");
+                res.statusCode = 200;
+
+                res.write(`<div>
+                <h1>List of users</h1>
+                <ol>
+                    ${list}
+                </ol>
+
+                <br>
+                <br>
+                <a href="/create-user">Create a new User</a>
+            </div>`);
+
+                return res.end();
+            }
+            res.setHeader("Content-Type", "text/html");
+            res.statusCode = 200;
+
+            res.write(`<div>
+                    <h1>List of users</h1>
+                    <ol>
+                        There are no users
+                    </ol>
+    
+                    <br>
+                    <br>
+                    <a href="/create-user">Create a new User</a>
+                </div>`);
+
+            return res.end();
+        });
+    }
 });
-
-app.listen(3000);
+server.listen(3000);
