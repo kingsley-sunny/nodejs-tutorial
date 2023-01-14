0801446574f3106e722cd87a205638938c4839b6
kingsley-sunny <ezeobisunny51@gmail.com>
2023-01-14 23:26:39 +0100

    [inserting data to mysql] --  implemented the code to insert data to mysql

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,8 +15,7 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
     const product = new Product(null, title, imageUrl, description, price);
-    product.save();
-    res.redirect("/");
+    product.save().then(val => res.redirect("/"));
 };
 
 exports.getEditProduct = (req, res, next) => {
